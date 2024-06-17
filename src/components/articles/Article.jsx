a75b938ab3a4dce268d1cc290143498b34415244
Samson Ng <samsonng@Samsons-iMac.local>
2024-06-17 16:31:01 +0100

    complete task 7

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getArticleByID, getCommentsByArticle } from '../../api';
+import { getArticleByID, getCommentsByArticle, patchVotes } from '../../api';
 import { CommentCard } from './CommentCard';
 
 export const Article = () => {
   const [article, setArticle] = useState({});
   const [commentList, setCommentList] = useState([]);
+  const [fakeKudos, setFakeKudos] = useState(0);
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -16,9 +17,28 @@ export const Article = () => {
 
     Promise.all(promiseArr).then((data) => {
       setArticle(data[0].article);
+      setFakeKudos(data[0].article.votes);
       setCommentList(data[1].comments);
     });
   }, []);
+
+  const handelThumbUp = () => {
+    setFakeKudos(fakeKudos + 1);
+    patchVotes(article_id, 1)
+      .then((data) => {})
+      .catch((err) => {
+        setFakeKudos(fakeKudos - 1);
+      });
+  };
+  
+  const handelThumbDown = () => {
+    setFakeKudos(fakeKudos - 1);
+    patchVotes(article_id, -1)
+      .then((data) => {})
+      .catch((err) => {
+        setFakeKudos(fakeKudos + 1);
+      });
+  };
 
   return (
     <main>
@@ -28,9 +48,16 @@ export const Article = () => {
             <img id="article-img" src={article.article_img_url} />
             <h2>{article.title}</h2>
             <h3>{article.author}</h3>
-            <p className="article-info">{`${article.topic.toUpperCase()} • ${
-              article.created_at
-            } • 👍 ${article.votes}`}</p>
+            <p className="article-info">
+              {`${article.topic.toUpperCase()} • ${article.created_at} • `}
+              <button className="kudos" onClick={handelThumbUp}>
+                👍
+              </button>{' '}
+              <button className="kudos" onClick={handelThumbDown}>
+                👎
+              </button>{' '}
+              {`${fakeKudos}`}
+            </p>
             <p>{article.body}</p>
           </div>
         ) : (
