c83ff0d2721ed71b1f3c2e2b7a028792b380bcb6
Samson Ng <samsonng@Samsons-iMac.local>
2024-06-18 19:23:12 +0100

    add pagination

--- src/components/articles/ArticleList.jsx
+++ src/components/articles/ArticleList.jsx
@@ -5,27 +5,90 @@ import { useParams } from 'react-router-dom';
 
 export const ArticleList = ({ sortBy, order }) => {
   const [articleList, setArticleList] = useState([]);
+  const [articlesOnThisPage, setArticlesOnThisPage] = useState([]);
   const [isArticleListLoading, setIsArticleListLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [articlePerPage, setArticlePerPage] = useState(10);
   const { topic } = useParams();
 
   useEffect(() => {
     getArticles({ topic, sortBy, order }).then((data) => {
       setArticleList(data.articles);
+      console.log(data.articles.length);
+      setArticlesOnThisPage(
+        data.articles.slice(
+          (currentPage - 1) * articlePerPage,
+          currentPage * articlePerPage
+        )
+      );
+
       setIsArticleListLoading(false);
     });
   }, [topic, sortBy, order]);
 
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+    setArticlesOnThisPage(
+      articleList.slice(
+        currentPage * articlePerPage,
+        (currentPage + 1) * articlePerPage
+      )
+    );
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage(currentPage - 1);
+    setArticlesOnThisPage(
+      articleList.slice(
+        (currentPage - 2) * articlePerPage,
+        (currentPage - 1) * articlePerPage
+      )
+    );
+  };
+
   return (
     <div>
+      <Pagination
+        currentPage={currentPage}
+        articlePerPage={articlePerPage}
+        articleList={articleList}
+        handleNext={handleNext}
+        handlePrevious={handlePrevious}
+      />
       {isArticleListLoading ? (
         <h2 className="loading-message">Loading articles...</h2>
       ) : (
         <ul id="article-list">
-          {articleList.map((article) => {
+          {articlesOnThisPage.map((article) => {
             return <ArticleCard key={article.article_id} {...article} />;
           })}
         </ul>
       )}
     </div>
+  );
+};
+
+const Pagination = (props) => {
+  const {
+    currentPage,
+    articlePerPage,
+    articleList,
+    handleNext,
+    handlePrevious,
+  } = props;
+  return (
+    <section id="pagination">
+      {currentPage === 1 ? null : (
+        <button onClick={handlePrevious} className="pagination-button">
+          Previous
+        </button>
+      )}
+      Page {currentPage} of {Math.ceil(articleList.length / articlePerPage)}
+      {currentPage === Math.ceil(articleList.length / articlePerPage) ? null : (
+        <button onClick={handleNext} className="pagination-button">
+          Next
+        </button>
+      )}
+    </section>
   );
 };
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,26 +14,32 @@ export const Nav = ({ topicList, handleSortByChange, handleOrderChange }) => {
           );
         })}
       </ul>
-      <form>
-        <label>
-          Sort by:{' '}
-          <select onChange={handleSortByChange}>
-            <option value="created_at">Date</option>
-            <option value="comment_count">Comment count</option>
-            <option value="votes">Votes</option>
-            <option value="title">Title</option>
-          </select>
-        </label>
-      </form>
-      <form>
-        <label>
-          Order by:{' '}
-          <select onChange={handleOrderChange}>
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-          </select>
-        </label>
-      </form>
+      <ul id="option-list">
+        <li>
+          <form>
+            <label>
+              Sort by:{' '}
+              <select onChange={handleSortByChange}>
+                <option value="created_at">Date</option>
+                <option value="comment_count">Comment count</option>
+                <option value="votes">Votes</option>
+                <option value="title">Title</option>
+              </select>
+            </label>
+          </form>
+        </li>
+        <li>
+          <form>
+            <label>
+              Order by:{' '}
+              <select onChange={handleOrderChange}>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </select>
+            </label>
+          </form>
+        </li>
+      </ul>
     </nav>
   );
 };
